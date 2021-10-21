65b66758372ff69b2be68b730cd540a5b7cf8064
Juan Carlos Lopez Casillas <JuanCarlos_Lopez@pegaduro.com>
2021-10-21 16:05:05 -0500

    Colores y logo de app

--- app/src/main/java/com/juan/notes/ui/MainActivity.kt
+++ app/src/main/java/com/juan/notes/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.juan.notes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         val navHostFragment = supportFragmentManager
